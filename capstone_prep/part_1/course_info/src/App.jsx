2b16b4ddbad376e78da8880b9c9c2f82d7101952
Alex Harnett <harnettalex@gmail.com>
2025-03-21 11:45:01 -0700

    React exercises

---
+++ capstone_prep/part_1/course_info/src/App.jsx
@@ -0,0 +1,48 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  let content = props.parts.map(part => <p>{part.name}: {part.exercises}</p>)
+  
+  return (
+    content
+  )
+}
+
+const Total = (props) => {
+  let total = props.parts.reduce((sum, part) => sum + part.exercises, 0)
+  
+  return (
+    <>
+      <p>Number of exercises: {total}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
+    ]
+
+  }
+  
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
--- capstone_prep/part_1/introdemo/src/App.jsx
+++ capstone_prep/part_1/introdemo/src/App.jsx
@@ -1,16 +1,62 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Display = ({counter}) => {
   return (
+    <div>{counter}</div>
+  )
+}
+
+const Button = ({onClick, text}) => {
+  return(
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
       <div>
-          <p>Hello world</p>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
 export default App
